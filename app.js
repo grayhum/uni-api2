9844ebde5bc4774f346b5b97c9bb500fe7a62871
mannsoo <mansoo@woowahan.com>
2017-10-28 15:42:13 +0900

    first test code

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello World! on Aws-Serveless"
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    "Output": "Hello World! on Aws-Serverless"
   });
 });
 
